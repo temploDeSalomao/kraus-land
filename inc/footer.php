d46731f81168b6faf02c08229d7090a1c44e2a69
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-19 18:41:06 -0300

    Collabrotext Commit

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <footer>
-	<div class="btn_esconder">Hidde bg</div>
+	<div class="btn_esconder">Hidde</div>
 	<script src="js/lib/jquery/jquery.min.js"></script>
 	<script src="js/app.js"></script>
 </footer>
