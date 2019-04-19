ec291c92dc373646172cf7518c4f0f62f869ecdc
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-19 17:07:20 -0300

    Collabrotext Commit

--- inc/header.php
+++ inc/header.php
@@ -15,7 +15,7 @@
 			</ul>
 		</nav>
 		<ul class="sociais-container">
-			<li class="center al-end flex"> <a class="center al-end flex" href="#"><i class="fa fa-facebook f-black  center al-end flex"></i></a></li>
+			<li class="center al-emd flex"> <a href="#"><i class="fa fa-facebook"></i></a></li>
 		</ul>
 	</div>
 </header>
