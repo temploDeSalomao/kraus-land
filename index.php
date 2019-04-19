d15369f5612779875a6fb269d919ba998252f3df
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-19 19:13:11 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 	<div class="main-bg active"></div>
 	<div class="first-bg"></div>
 	<?php include 'inc/header.php'?>
-	
+	<main>
+		
+	</main>
+
 	<?php include 'inc/footer.php'?>
 </body>
 </html>
