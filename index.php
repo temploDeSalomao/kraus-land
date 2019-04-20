c814373308d7f7012d91f98b30f781a762e48dec
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-19 22:11:42 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 			<h1 class="uppercase f-white col-sm-12 col-md-12 no-padding pad-le_35 main-title compressed medium">Prensa-cabos metálicos</h1>
 			
 				
-			<article class="col-sm-12 col-md-6 pad-le_35 pad-top_5">
+			<article class="col-sm-12 col-md-6 pad-le_35 pad-top_5 lighter">
 				<h1 class="f-white_g">titulo</h1>
 			</article>
 			<aside  class="col-sm-12 col-md-6">
