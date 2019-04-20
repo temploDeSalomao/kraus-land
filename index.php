1b4c39f69a139f46ba33aad95b18372e238f7d34
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-19 22:40:01 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 			</header>
 			
 			<p class="col-sm-12 col-md-6 pad-le_35 pad-top_25 lighter f-white lighter flex">
-				teste
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam nemo illum, rerum vitae iusto! Cumque impedit, deleniti ducimus obcaecati, fugit mollitia asperiores blanditiis, minima nesciunt, odit quos! Molestiae, eum itaque.
 			</p>
 			<aside  class="col-sm-12 col-md-6">
 				<figure>
