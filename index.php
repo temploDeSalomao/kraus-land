04354dd761a66d8c001276d3e2069941140430b9
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-19 22:31:38 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 	<?php include 'inc/header.php'?>
 	<main>
 		<section class="container-fluid container-header">
-			<h1 class="uppercase f-white col-sm-12 col-md-12 no-padding pad-le_35 main-title compressed medium">Prensa-cabos metálicos</h1>
+			<header class="col-sm-12 col-md-12 no-padding pad-le_35">
+				<h1 class="uppercase f-white  main-title compressed medium">Prensa-cabos metálicos</h1>
+			</header>
 			
 			<article class="col-sm-12 col-md-6 pad-le_35 pad-top_25 lighter">
 				<h1 class="f-white lighter">titulo</h1>
@@ -18,7 +20,6 @@
 			
 		</section>
 	</main>
-
 	<?php include 'inc/footer.php'?>
 </body>
 </html>
