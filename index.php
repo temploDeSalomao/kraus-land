231a5cdd55cb15c6236aa3d0102ebf45e5eb9d6d
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-20 19:06:31 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 						</figure>
 					</li>
 				</ul>
-				<a href="" class="btn uppercase f-white "></a>
+				<a href="" class="btn uppercase f-white bg-green"></a>
 			</div>
 			<figure class="col-sm-12 col-md-5 flex center al-center no-padding">
 				<img class="flex al-center" src="images/body/prensa-cabos.png" alt="">
