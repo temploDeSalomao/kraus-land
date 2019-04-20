f1711dec11194c05507c45f95703bbe5a72fc830
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-20 18:25:47 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 				</ul>
 				<ul class="container-certified flex row nowrap">
 					<li>
-						<figure class="flex column reverse">
+						<figure class="flex column reverse no-grow">
 							<img src="images/logo/rohs-compliant.png" alt="">
 							<figcaption>Conformidade</figcaption>
 						</figure>
