520ad6ea5c0a3abfc775b8f52bd313eb302c2994
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-20 01:04:29 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<div class="first-bg"></div>
 	<?php include 'inc/header.php'?>
 	<main>
-		<section class="container-fluid container-md first-section  ">
+		<section class="container-fluid container-md first-section flex row wrap ">
 			<header class="col-sm-12 col-md-12 pad-le_35">
 				<h1 class="uppercase f-white main-title compressed medium">Prensa-cabos metálicos</h1>
 			</header>
