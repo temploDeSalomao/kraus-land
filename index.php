89574acf3e058767f879d28fbb903c3d7300d59f
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-20 20:53:41 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 			<div class="col-sm-12 col-md-6 article-background"></div>
 		</div>
 		<div class="container-fluid certification-divisor">
-			<div class="col-sm-12 col-md-6 certification bg-gray-light uppercase">
+			<div class="col-sm-12 col-md-6 certification bg-black uppercase">
 				certificações
 			</div>
 			<div class="col-sm-12 col-md-6 sponsorship bg-gray-bold">
