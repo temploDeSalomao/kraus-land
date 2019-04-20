a3a647ed54999efd242b872db97cb367f64b9eb8
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-20 12:34:57 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -29,6 +29,17 @@
 				<img src="images/body/prensa-cabos.png" alt="">
 			</figure>
 		</section>
+		<div class="container-fluid about-enterprise col-sm-12">
+			<article class="col-sm-12 col-md-6 bg-gray">
+				<h1 class="article-title f-gray uppercase">Kraus-Muller</h1>
+				<h2 class="article-sub-title f-white uppercase ">Conheça a nossa história</h2>
+				<p>Com objetivos alinhados à sua estratégia de atuação e a processos inovadores, a Kraus - Muller Indústria e Comércio, vem, desde sua fundação em 1999, conquistando o mercado com a qualidade e funcionalidade de seus produtos, aliados à excelência de seu atendimento ao cliente.</p>
+				<p>Com objetivos alinhados à sua estratégia de atuação e a processos inovadores, a Kraus - Muller Indústria e Comércio, vem, desde sua fundação em 1999, conquistando o mercado com a qualidade e funcionalidade de seus produtos, aliados à excelência de seu atendimento ao cliente.</p>
+				<p>Com objetivos alinhados à sua estratégia de atuação e a processos inovadores, a Kraus - Muller Indústria e Comércio, vem, desde sua fundação em 1999, conquistando o mercado com a qualidade e funcionalidade de seus produtos, aliados à excelência de seu atendimento ao cliente.</p>
+			</article>
+			<!-- When i try to use img here dont have the same effect that i want -->
+			<div class="col-sm-12 col-md-6 article-background"></div>
+		</div>
 	</main>
 	<?php include 'inc/footer.php'?>
 </body>
