9cf33e0cb72d55538a51fe1edb83676c34366844
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-21 20:37:01 -0300

    Collabrotext Commit

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,14 +10,15 @@ $(window).ready(function(){
 // Change div content - pour products
 
 $('.tab-aside').click(function(){
-	$('.tab-aside').each(function(){
+	$('.tab-aside').each(function(x){
 			if($(this).hasClass('active')){
 			$(this).removeClass('active');
+			x.stopPropagation();
 		}
 		else{
 			$(this).addClass('active');
 		}
-	}) 
+	});
 	$('.tabela').each(function(){
 		if($(this).hasClass('active')){
 			$(this).removeClass('active');
