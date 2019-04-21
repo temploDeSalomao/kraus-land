57230c5fcbd2833d3ad6e1f334ddf65279ab4676
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-21 17:32:05 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
 							<span class="about-product-subtitle f-yellow compressed">Modelo Prensa Cabo Metálico Rosca PG</span>
 							<p>Esse modelo foi criado na Alemanha e seu nome é a abreviação de Stahl-Panzer-Rohr-Gewind. Dessa forma, seus tamanhos são nomeados pelo prefixo PG. A linha metálica apresenta resistência mecânica superior aos prensa cabos de plásticos.</p>
 							<span class="about-product-subtitle f-yellow compressed">Informações Gerais:</span>
-							<ul class="flex coloumn no wrap f-white no-decorate">
+							<ul class="flex column no wrap f-white no-decorate">
 								<li class="f-white"><b>Material:</b> latão niquelado. PA 5 e borracha injetável;</li>
 								<li class="f-white"><b>Norma:</b> DIN 46320 - EN 50282;</li>
 								<li class="f-white"><b>Proteção:</b> Ip 68;</li>
