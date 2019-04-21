71c7671463e0061181d6b9b9476bf9550f96042b
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-21 17:56:00 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
 						<figure class="col-sm-12 col-md-6 flex column wrap start product-pic no-padding">
 							<img src="images/products/rosca-pg.jpg" alt="Rosca Pg">
 							<figcaption>
-								<div class="product-images flex row nowrap space-around">
+								<div class="product-images flex row nowrap space-between">
 									<img src="images/products/rosca-pg.jpg" alt="">
 									<img src="images/products/rosca-pg.jpg" alt="">
 									<img src="images/products/rosca-pg.jpg" alt="">
