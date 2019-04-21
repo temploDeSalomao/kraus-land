ac8c97003f270d262c1901b54f4c17a22b83afa0
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-21 16:50:37 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
 			<div class="col-sm-12 col-md-12">
 
 				<section class="col-sm-12 col-md-6 flex row reverse">
-					<header class="flex column wrap al-center">
+					<header class="flex column wrap al-center center">
 						<h4 class="bold f-white">teste</h4>
 					</header>
 
