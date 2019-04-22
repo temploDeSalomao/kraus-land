dfd92b0653ef7990f55317723c66055faaf8ed69
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-21 22:58:45 -0300

    Collabrotext Commit

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,6 @@
 $(window).ready(function(){
 	$('.btn_esconder').click(function(x){
-			$('.main-bg').toggleClass('active');
+		$('.main-bg').toggleClass('active');
 		$(this).val('Show');
 		x();
 	});
