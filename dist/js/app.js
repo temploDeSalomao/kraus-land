ab9b96bb06582b0fd44c004062024ef034009fa4
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-21 22:15:16 -0300

    Collabrotext Commit

--- dist/js/app.js
+++ dist/js/app.js
@@ -72,73 +72,73 @@ var myChart = new Chart(ctx, {
 });
 
 
-var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-		var config = {
-			type: 'line',
-			data: {
-				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-				datasets: [{
-					label: 'My First dataset',
-					backgroundColor: window.chartColors.red,
-					borderColor: window.chartColors.red,
-					data: [
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor()
-					],
-					fill: false,
-				}, {
-					label: 'My Second dataset',
-					fill: false,
-					backgroundColor: window.chartColors.blue,
-					borderColor: window.chartColors.blue,
-					data: [
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor()
-					],
-				}]
-			},
-			options: {
-				responsive: true,
-				title: {
-					display: true,
-					text: 'Chart.js Line Chart'
-				},
-				tooltips: {
-					mode: 'index',
-					intersect: false,
-				},
-				hover: {
-					mode: 'nearest',
-					intersect: true
-				},
-				scales: {
-					xAxes: [{
-						display: true,
-						scaleLabel: {
-							display: true,
-							labelString: 'Month'
-						}
-					}],
-					yAxes: [{
-						display: true,
-						scaleLabel: {
-							display: true,
-							labelString: 'Value'
-						}
-					}]
-				}
-			}
-		};
+// var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+// 		var config = {
+// 			type: 'line',
+// 			data: {
+// 				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+// 				datasets: [{
+// 					label: 'My First dataset',
+// 					backgroundColor: window.chartColors.red,
+// 					borderColor: window.chartColors.red,
+// 					data: [
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor()
+// 					],
+// 					fill: false,
+// 				}, {
+// 					label: 'My Second dataset',
+// 					fill: false,
+// 					backgroundColor: window.chartColors.blue,
+// 					borderColor: window.chartColors.blue,
+// 					data: [
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor(),
+// 						randomScalingFactor()
+// 					],
+// 				}]
+// 			},
+// 			options: {
+// 				responsive: true,
+// 				title: {
+// 					display: true,
+// 					text: 'Chart.js Line Chart'
+// 				},
+// 				tooltips: {
+// 					mode: 'index',
+// 					intersect: false,
+// 				},
+// 				hover: {
+// 					mode: 'nearest',
+// 					intersect: true
+// 				},
+// 				scales: {
+// 					xAxes: [{
+// 						display: true,
+// 						scaleLabel: {
+// 							display: true,
+// 							labelString: 'Month'
+// 						}
+// 					}],
+// 					yAxes: [{
+// 						display: true,
+// 						scaleLabel: {
+// 							display: true,
+// 							labelString: 'Value'
+// 						}
+// 					}]
+// 				}
+// 			}
+// 		};
 
 
 });
