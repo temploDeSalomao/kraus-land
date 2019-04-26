3af6ada3b471d6e25fc6a9bb0ff86743d7dcba38
Vitor Lima <vitorblsantos@gmail.com>
2019-04-26 18:59:52 -0300

    another land inside same project

---
+++ second.php
@@ -0,0 +1,55 @@
+<?php include 'inc/head.php'?>
+<body>
+	<?php include 'inc/header_2.php'?>
+	<main>
+		<section class="container-fluid container-md first-section flex row wrap ">
+			<header class="col-sm-12 col-md-6">
+				<h1 class="uppercase f-white main-title compressed medium">Prensa-cabos metálicos</h1>
+			</header>
+			<div class="clearfix"></div>
+			<figure class="col-sm-12 col-md-5 flex center al-center no-padding header-image hidden-md">
+				<img class="flex al-center" src="images/body/prensa-cabos.png" alt="">
+			</figure>
+			<figure class="col-sm-12 col-md-5 flex center al-center no-padding header-image hidden-sm">
+				<img class="flex al-center" src="images/body/prensa-cabos.png" alt="">
+			</figure>
+			<div class="col-sm-12 col-md-7 pad-le_352 pad-top_15 f-white_g compressed flex column wrap no-grow text-shadow">
+				<p class="main-subtitle" >A linha de prensa-cabos metálicos da Kraus-Muller foi desenvolvida para proteger a sua instalação elétrica, evitando contato com a água, poeira e evitando perdas de energia com anéis de vedação e por ser metálica essa linha apresenta resistência mecânica superior aos presa-cabos de termoplástico. Para garantir a  vedação em suas instalações eĺétricas, conte com a linha Kraus-Muller!</p>
+				
+				<h3 class="medium uppercase text-shadow">Vantagens:</h3>
+				<ul class="container-vantagens flex column wrap al-start space-around no-decorate" >
+					<li class='box-ticked compressed flex nowrap start al-start '>Protege os equipamentos contra possiveis riscos de curto-circuito;</li>
+					<li class='box-ticked compressed flex nowrap start al-start '>Blindado contra açao eletromagnética;</li>
+					<li class='box-ticked compressed flex nowrap start al-start '>Alto poder de vedação e travamento para evitar a tensão do cabo e a tração por causa do peso do fio.</li>
+				</ul>
+				<ul class="container-certified flex row nowrap al-start start pad-le_1">
+					<li class="certified" >
+						<figure class="flex column reverse no-grow full-basis">
+							<img src="images/logo/rohs-compliant.png" alt="">
+							<figcaption class="flex center ">Conformidade</figcaption>
+						</figure>
+					</li>
+					<li class="certified">
+						<figure class="flex column reverse ">
+							<img class="flex  center full-basis ce " src="images/logo/CE.png" alt="">
+							<figcaption class="flex center ">Certificação</figcaption>
+						</figure>
+					</li>
+					<li class="certified">
+						<figure class="flex column reverse no-grow full-basis">
+							<img src="images/logo/reciclavel.png" alt="">
+							<figcaption class="flex center ">Reciclável</figcaption>
+						</figure>
+					</li>
+					<li class="certified">
+						<figure class="flex column reverse no-grow full-basis">
+							<img src="images/logo/sustentavel.png" alt="">
+							<figcaption class="flex center ">Sustentável</figcaption>
+						</figure>
+					</li>
+				</ul>
+				<a href="" class="btn-large uppercase f-white bg-green">Quero solicitar um orçamento</a>
+			</div>
+		</section>
+	</main>
+</body>
