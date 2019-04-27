ff1b1258c330f1ccff8afa56a92a2b78df5d712b
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-27 19:09:15 -0300

    Collabrotext Commit

--- dist/js/app.js
+++ dist/js/app.js
@@ -65,7 +65,7 @@ $('.tab-aside').click(function(){
 		x = $(window).scrollTop();
 		if(x>180){
 			$('.header-home').addClass('fixed');
-			$('html,body').css('padding-top','150px');
+			$('html,body').css('padding-top','120px');
 		}
 		else{
 			$('.header-home').removeClass('fixed');
