fc61b70116c73968cec4908a837a977d8fa9901a
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 16:09:23 -0300

    Collabrotext Commit

--- inc/controller/engine.php
+++ inc/controller/engine.php
@@ -6,5 +6,5 @@ $telefone =isset ($_POST['telefone']) ? $_POST['telefone']  : '';
 $produto =isset ($_POST['produto']) ? $_POST['produto']  : '';
 $modelo =isset ($_POST['modelo']) ? $_POST['modelo']  : '';
 $quantidade =isset ($_POST['quantidade']) ? $_POST['quantidade']  : '';
-
+$mensagem =isset ($_POST['mensagem']) ? $_POST['mensagem']  : '';
 
