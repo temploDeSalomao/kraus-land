17fac48388cfec5cca3c6472058fb5e0535bd67e
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 15:36:03 -0300

    Collabrotext Commit

--- dist/js/app.js
+++ dist/js/app.js
@@ -109,7 +109,7 @@ $('.anchor').click(function(x){
 // string
 
 $('input[name="telefone"]').blur(function(x){
-	$(this).html='';
+	$(this).html('');
 	x=$(this).val().replace(/(\d{2})(\d{5})(\d{4})/,"$1-$2-$3");
 	
 })
