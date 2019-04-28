573b7cc52e2085f9ef8e7fdfe603fd8d138efad3
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 15:54:17 -0300

    Collabrotext Commit

--- dist/js/app.js
+++ dist/js/app.js
@@ -113,7 +113,7 @@ $('.anchor').click(function(x){
 function lockBTN(){
 	$("input[type='submit']").attr("Disabled","true");
 	$("input[type='submit']").css({"cursor":"not-allowed","background":"#9f6916","border-color":"#9f6916"});
-	$(".container-submit").hover(function(){$(this + ' *').css("color","#000")});
+	$(".container-submit").hover(function(){$(this.childrens()).css("color","#000")});
 }
 
 // mask tel
