d59bc9464e85e2ecd890dfe44cafd73cde99e069
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 14:26:29 -0300

    Collabrotext Commit

--- inc/view/fast-actions.php
+++ inc/view/fast-actions.php
@@ -1,6 +1,6 @@
 <div class="main-fast-actions">
 	<div class="overlay"></div>
-	<div class="open-wpp flex column wrap center alc-center">
+	<div class="open-wpp flex column wrap center al-center">
 		<button class="close-modal flex end al-end">X</button>
 		<i class="f-yellow fa fa-whatsapp"></i><br>
 		<span class="lighter f-gray">envie um <br> <span class="medium f-white">Whatsapp!</span></span>
