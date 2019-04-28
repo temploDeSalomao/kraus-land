993e730444c963a549f69e73567030ba0fc92df4
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 16:27:01 -0300

    Collabrotext Commit

--- inc/controller/engine.php
+++ inc/controller/engine.php
@@ -2,7 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors','On');
 require_once 'mailler/config.php'; 
-
 global $acao, $nome, $email, $telefone, $produto, $modelo, $quantidade, $mensagem;
 
 
