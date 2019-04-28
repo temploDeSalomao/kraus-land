1fb1bd77a554593ceb4ccb3ed266de482d4bc501
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 16:42:05 -0300

    Collabrotext Commit

--- inc/controller/engine.php
+++ inc/controller/engine.php
@@ -16,7 +16,7 @@ $GLOBALS['mensagem']=isset ($_POST['mensagem']) ? $_POST['mensagem']  : '';
 
 switch ($acao) {
 	case 'form':
-			$mensagem = "Contato através do site. \n".$GLOBALS['nome'];
+			$mensagem = "Contato através do site. <br>".$GLOBALS['nome'];
 			echo $mensagem; 
 		break;
 }
