e0e3b5610ca548dc45fe13e5700d2d3a85a756d7
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 16:45:57 -0300

    Collabrotext Commit

--- inc/controller/engine.php
+++ inc/controller/engine.php
@@ -16,7 +16,7 @@ $GLOBALS['mensagem']=isset ($_POST['mensagem']) ? $_POST['mensagem']  : '';
 
 switch ($acao) {
 	case 'form':
-			$mensagem = "<span><b>Informações do contato</b><br> <i>Nome do produto</i>: ".$GLOBALS['produto']."&nbsp;&nbsp;&nbsp;<i>Modelo</i>: </span>";
+			$mensagem = "<span style='font-size: 12px;'><b>Informações do contato</b><br> <i>Nome do produto</i>: ".$GLOBALS['produto']."&nbsp;&nbsp;&nbsp;<i>Modelo</i>: </span>";
 			echo $mensagem; 
 		break;
 }
