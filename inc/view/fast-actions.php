693e53f3f50f8d7e8ca9b64f6bb9b2ccde95cd9a
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 20:01:38 -0300

    Collabrotext Commit

--- inc/view/fast-actions.php
+++ inc/view/fast-actions.php
@@ -7,8 +7,8 @@
 			<span class="lighter f-white opaque">envie um <br> <b class="medium f-white">Whatsapp!</b></span>
 		</a>
 	</div>
-	<div class="outer-open-modal flex column wrap center al-center">
-		<div class="content-open-modal">
+	<div class="outer-open-modal">
+		<div class="content-open-modal flex column wrap center al-center">
 			<span class="flex row wrap center al-center f-white lighter"><b>Olá! </b><br>Deixe o seu contato, retornaremos para você!</span>
 			<button class="open-modal bg-yellow">SIM</button>
 		</div>
