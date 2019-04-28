ef4c76ea49a52c5095c41ddaa95de9e37295e6f9
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 16:36:32 -0300

    Collabrotext Commit

--- inc/controller/engine.php
+++ inc/controller/engine.php
@@ -29,7 +29,7 @@ switch ($acao) {
 		$GLOBALS['mensagem'] = "Contato através do site. Mensagem do usuário:&nbsp;"$GLOBALS['nome'] .'\r\n'.  $GLOBALS['mensagem'];
 		break;
 	case 'form':
-		$GLOBALS['mensagem'] = "Contato através do site.\r\n Tipo De Produto:".$GLOBALS['produto']."&nbsp;&nbsp; Modelo:".$GLOBALS['modelo']."&nbsp;&nbsp; Quantidade:".$GLOBALS['modelo']."&nbsp;&nbsp;Mensagem:&nbsp;".$GLOBALS['nome'] .'\r\n'.  $GLOBALS['mensagem'];
+		$GLOBALS['mensagem'] = "Contato através do site. Tipo De Produto:".$GLOBALS['produto']."&nbsp;&nbsp; Modelo:".$GLOBALS['modelo']."&nbsp;&nbsp; Quantidade:".$GLOBALS['modelo']."&nbsp;&nbsp;Mensagem:&nbsp;".$GLOBALS['nome'] .'\r\n'.  $GLOBALS['mensagem'];
 		break;		
 }
 
