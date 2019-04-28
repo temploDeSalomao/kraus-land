44ffeb01237e561e485e44ba6015136b40829348
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 14:19:44 -0300

    Collabrotext Commit

--- inc/view/fast-actions.php
+++ inc/view/fast-actions.php
@@ -1,4 +1,4 @@
-<div class="container-fast-actions">
+<div class="main-fast-actions">
 	<div class="overlay"></div>
 	<div class="open-wpp">
 		<button class="close-modal flex end al-end">X</button>
