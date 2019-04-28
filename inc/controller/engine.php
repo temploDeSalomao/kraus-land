dcf646e33aca1849e6da5485bdef344c1911a30b
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 16:50:05 -0300

    Collabrotext Commit

--- inc/controller/engine.php
+++ inc/controller/engine.php
@@ -18,7 +18,7 @@ switch ($acao) {
 	case 'form':
 			$mensagem = "<b>Informações do contato</b><span style='font-size: 12px;'><br> <i>Nome do produto</i>: ".$GLOBALS['produto'].";&nbsp;&nbsp;&nbsp;<i>Modelo</i>:". 
 			$GLOBALS['modelo'] ."&nbsp;&nbsp; Quantidade:".$GLOBALS['quantidade']."</span><br>
-			<b>Mensagem</b>";
+			<b>Mensagem:</b>";
 		break;
 }
 
