cb4d6885b1305753413b169d90c9cdeeda8db173
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-28 15:18:27 -0300

    Collabrotext Commit

--- index.php
+++ index.php
@@ -464,7 +464,8 @@
 									<option value="2">2</option>
 									<option value="3">3</option>
 									<option value="4">4</option>
-									<option value="+10">+10</option>
+									<option value="5">5</option>
+									<option value="+5">+5</option>
 									<option disabled selected >Quantidade</option>
 								</select>
 							</label>
